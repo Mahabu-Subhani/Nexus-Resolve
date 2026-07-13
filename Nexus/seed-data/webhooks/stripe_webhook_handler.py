f32d9e26427ff593d29117c05b691bf804284ff0
MD Mahabu Subhani <139764009+Mahabu-Subhani@users.noreply.github.com>
2026-07-14 02:59:03 +0530

    Add files via upload

---
+++ Nexus/seed-data/webhooks/stripe_webhook_handler.py
@@ -0,0 +1,12 @@
+﻿import stripe
+from django.conf import settings
+from billing.charge_service import ChargeService
+from billing.subscription_manager import SubscriptionManager
+
+def handle_stripe_webhook(payload, sig_header):
+    event = stripe.Webhook.construct_event(payload, sig_header, settings.STRIPE_WEBHOOK_SECRET)
+    if event["type"] == "charge.succeeded":
+        ChargeService().get_charge_status(event["data"]["object"]["id"])
+    elif event["type"] == "invoice.payment_failed":
+        SubscriptionManager().list_invoices(event["data"]["object"]["customer"])
+    return event
